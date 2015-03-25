3c0acf4e39a85f3cf96509a30160f72c446de082
Fredrik Ekelund <bohemenian@gmail.com>
2015-03-25 10:20:47 +0100

    Bumped version to 1.0.3
    Did it to be able to publish the package for Windows

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
 	name: "fredrik:node-xmpp",
 	summary: "Wrapper for the node-xmpp npm package",
-	version: "1.0.0",
+	version: "1.0.3",
 	git: "https://github.com/fredrikekelund/meteor-node-xmpp.git"
 });
 
-Npm.depends({"node-xmpp": "1.0.0-alpha2"});
+Npm.depends({"node-xmpp": "1.0.0-alpha1"});
 
 Package.onUse(function(api) {
 	api.addFiles("xmpp.js", "server");
